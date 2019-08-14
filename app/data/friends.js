4972960a37b9bf705044dd0f4cf1c4b4373ec3bb
CJ Hubbard <mysticriver84@gmail.com>
2019-08-13 21:35:16 -0400

    finalized html, css, js, routes

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friendsList = [
+	{
+	  name: "Joe",
+	  photo: "https://media.giphy.com/media/qYr8p3Dzbet5S/giphy.gif",
+	  preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+	},
+
+	{
+	  name: "Bethel",
+	  photo: "https://media.giphy.com/media/bKebPt6wpCPfO/giphy.gif",
+	  preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+	},
+
+	{
+	  name: "Bruno",
+	  photo: "https://media.giphy.com/media/ciqncHG6679Wo2aLyK/giphy.gif",
+	  preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    },
+    
+    {
+        name: "Gabe",
+        photo: "https://media.giphy.com/media/oz3CZLlmP9rji/giphy.gif",
+        preferences: [4, 5, 4, 1, 4, 3, 4, 4, 2, 4]
+      },
+];
+
+module.exports = friendsList;
